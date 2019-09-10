2fe6993de5f6bb3e816cf09c84c2dc6f8238a55e
leeannb <54560963+leeannb@users.noreply.github.com>
2019-09-09 20:21:19 -0400

    Add files via upload

---
+++ Assignment1pg7.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd 
+Location = "all_040_in_12.P1.csv"
+df = pd.read_csv(Location)
+
+
+# In[2]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
